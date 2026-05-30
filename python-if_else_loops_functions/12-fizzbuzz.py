7c637830d5994bcfa9e90481b6c42240b4f639bc
Elvin <14517@holbertonstudents.com>
2026-05-30 15:23:23 +0400

    python213

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """Print numbers 1 to 100 with Fizz/Buzz rules, each followed by a space."""
+    """Print numbers from 1 to 100 with Fizz/Buzz rules.
+
+    Each element is followed by a space.
+    """
     for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz", end=" ")
